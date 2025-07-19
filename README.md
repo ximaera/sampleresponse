ab63fdc67fd8feb9329ad5e0608b93d27006f798
Toema Gavrichenkov <ximaera@gmail.com>
2025-07-19 17:00:07 +0300

    Added an env variable for the HTTP listening port

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Usage:
 - `go build`
 - `./sampleresponse`
 - `curl -H "Host: 503.status.http.example.com" http://localhost:8080`
-- optionally, set env variable BASE_DOMAIN to something other than "example.com"
+- optionally, set env variable `BASE_DOMAIN` to something other than "example.com"
+- optionally, set env variable `HTTP_PORT` to something other than "8080"
 
 MIT license
 
--- main.go
+++ main.go
@@ -6,13 +6,18 @@ import (
 )
 
 var baseDomain = "example.com" // env BASE_DOMAIN
+var httpPort = "8080"          // env HTTP_PORT
 
 func main() {
     if envDomain := os.Getenv("BASE_DOMAIN"); envDomain != "" {
         baseDomain = envDomain
     }
 
-    if err := startHTTPServer(":8080"); err != nil {
+    if envHttpPort := os.Getenv("HTTP_PORT"); envHttpPort != "" {
+        httpPort = envHttpPort
+    }
+
+    if err := startHTTPServer(":" + httpPort); err != nil {
         fmt.Println("Error starting HTTP server:", err)
         os.Exit(1)
     }
